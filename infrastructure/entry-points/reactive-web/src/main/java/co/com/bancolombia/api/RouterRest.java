a40160f7223785ca0fd4e5ab99b6209964498085
MalloriLopez <mallorilopez1@gmail.com>
2025-09-08 00:25:20 -0500

    HU4-listar_solicitudes - ajustes a la documentacion con swagger

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
@@ -5,10 +5,14 @@ import co.com.bancolombia.api.dto.response.LoanApplicationListResponse;
 import co.com.bancolombia.api.dto.response.LoanApplicationResponseDTO;
 import co.com.bancolombia.model.loanapplication.LoanApplicationReviewItem;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +23,12 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class RouterRest {
     @Bean
     @RouterOperations({
@@ -47,24 +57,27 @@ public class RouterRest {
             ),
             @RouterOperation(
                     path = "/api/v1/solicitud",
-                    method = RequestMethod.POST,
+                    method = RequestMethod.GET,
                     beanClass = Handler.class,
                     beanMethod = "listLoanApplicationsUseCase",
                     operation = @Operation(
                             operationId = "listLoanApplications",
-                            summary = "Lista las solicitudes de prestamo pendientes por revision",
+                            summary = "Lista las solicitudes de prestamo pendientes por revision con paginacion y filtros",
                             description = "Lista solicitudes de prestamos y devuelve su representación",
-                            requestBody = @RequestBody(
-                                    required = true,
-                                    content = @Content(schema = @Schema(implementation = LoanApplicationReviewItem.class))
-                            ),
+                            security = { @SecurityRequirement(name = "bearerAuth") },
+                            parameters = {
+                                    @Parameter(name = "page", description = "Page number", example = "0"),
+                                    @Parameter(name = "size", description = "Page size", example = "10"),
+                                    @Parameter(name = "loanTypeId", description = "Filter by loan type", example = "1"),
+                                    @Parameter(name = "status", description = "Filter by status loan applications", example = "1")
+                            },
                             responses = {
                                     @ApiResponse(
                                             responseCode = "200",
-                                            description = "Solicitudes de prestamos listadas",
+                                            description = "Successfully retrieved list of applications",
                                             content = @Content(schema = @Schema(implementation = LoanApplicationListResponse.class))
                                     ),
-                                    @ApiResponse(responseCode = "400", description = "Petición inválida")
+                                    @ApiResponse(responseCode = "400", description = "Invalid parameters")
                             }
                     )
             )
